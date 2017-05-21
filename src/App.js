f01a1b04306e7fb27cad6c0834e91c8f29fe43f5
Chelsea Li <chenyaoli19@gmail.com>
2017-05-21 11:35:27 -0700

    a few adjustment. added chinese navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {BrowserRouter as Router,
         Route,
-        NavLink,
         Switch,
         Redirect
 } from 'react-router-dom';
 import Header from './shared/header/Header'
 import Footer from './shared/footer/Footer'
-const Home = () => <h1>Home</h1>
-const About = () => <h1>About</h1>
-const Contact = () => <h1>Contact</h1>
+import Home from './features/home'
+import CreateStory from './features/createStory'
 
+// const Home = () => <h1>Home</h1>
+const About = () => <h1>关于</h1>
 const App = () => (
   <Router>
     <div>
       <Header/>
       <Switch>
         <Route path="/home" component = {Home}/>
+        <Route path="/createStory" component = {CreateStory} />
         <Route path="/about" component = {About}/>
-        <Route path="/contact" component = {Contact}/>
         <Route path="/events/:year(\d{4})" render={({match}) =>(
             <h1>Detailed event list of year: {match.params.year}</h1>
           )}/>
