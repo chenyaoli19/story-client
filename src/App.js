640a9c0fd8df7e30b93223fa9db43634b9ed785e
Chelsea Li <chenyaoli19@gmail.com>
2017-05-21 16:27:41 -0700

    added a story event list page. initial hook-up with redux

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from './shared/footer/Footer'
 import Home from './features/home'
 import CreateStory from './features/createStory'
 import About from './features/about'
+import EventStoryList from './features/event-story-list'
 
 const App = () => (
   <Router>
@@ -17,6 +18,7 @@ const App = () => (
       <Switch>
         <Route path="/home" component = {Home}/>
         <Route path="/createStory" component = {CreateStory} />
+        <Route path="/event-story-list" component = {EventStoryList}/>
         <Route path="/about" component = {About}/>
         <Route path="/events/:year(\d{4})" render={({match}) =>(
             <h1>Detailed event list of year: {match.params.year}</h1>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import configureStore from './store/configure.store.js'
+import { Provider } from 'react-redux'
+
+const store = configureStore()
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
